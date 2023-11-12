b1246bfdd4f1506e1769746ee9a106be05ba4650
Abigail Fleming <abigail.fleming@csuglobal.edu>
2023-11-12 09:22:12 -0500

    adding program to add/subtract two numbers and another program to multiply/divide two numbers.

---
+++ add_subtract.py
@@ -0,0 +1,11 @@
+print('Enter number x: ')
+x = int(input())
+
+print('Enter number y: ')
+y = int(input())
+
+print('x + y = ', x+y)
+print('x - y = ', x-y)
+
+
+
---
+++ multiply_divide.py
@@ -0,0 +1,8 @@
+print('Enter number x: ')
+x = int(input())
+
+print('Enter number y: ')
+y = int(input())
+
+print('x * y = ', x*y)
+print('x / y = ', x/y)
